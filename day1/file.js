efe638dcf30ae72848030a446a9ac1a58bba267a
prashantbhandary <santhprashant@gmail.com>
2024-10-05 12:13:46 +0545

    day1 files

--- day1/file.js
+++ day1/file.js
@@ -1,8 +1,22 @@
 const fs= require("fs");
+// const os = require("os")
+// console.log(os.cpus().length) finding the core of computer
 
 // fs.writeFileSync("./text.txt","hello, its me prahsant")
 
 // fs.writeFile("./text.txt","heyy file", (err)=>{})
 
-const result= fs.readFileSync("./text.txt","utf-8")
-console.log(result)
+// const result= fs.readFileSync("./text.txt","utf-8")
+// console.log(result) //blocking
+
+fs.readFile("./text.txt","utf-8", (err, result)=>  //non blocking - asychronous
+{
+    if (err){
+        console.log("Error:" , err);
+    }
+    else {
+        console.log(result);
+     }
+        }
+)
+
